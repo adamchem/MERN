2e16eaa2749b19feff87917eb646b87540f5e757
adamchem <adam.chen@kimo.com>
2017-03-25 00:53:05 +0800

    update

--- HW4/Finished/app.js
+++ HW4/Finished/app.js
@@ -7,6 +7,9 @@ var file2 = fs.readFileSync(__dirname + '/Web_forn.html', 'utf8');
 var err404 = fs.readFileSync(__dirname + '/err404.html', 'utf8');
 var port = process.env.PORT || 3000;
 
+app.use('/pic', express.static(__dirname + '/pic'));
+app.use('/css', express.static(__dirname + '/css'));
+
 app.get('/', function(req, res) {
 	res.send(file1);
 });
