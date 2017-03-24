939eac2a5886dd82455c001c5460949eb15329b7
adamchem <adam.chen@kimo.com>
2017-03-24 23:18:20 +0800

    hw4

---
+++ HW4/Finished/app.js
@@ -0,0 +1,22 @@
+var express = require('express');
+var fs = require('fs');
+
+var app = express();
+var file1 = fs.readFileSync(__dirname + '/Web.html', 'utf8');
+var file2 = fs.readFileSync(__dirname + '/Web_forn.html', 'utf8');
+var err404 = fs.readFileSync(__dirname + '/err404.html', 'utf8');
+var port = process.env.PORT || 3000;
+
+app.get('/', function(req, res) {
+	res.send(file1);
+});
+
+app.get('/form', function(req, res) {
+	res.send(file2);
+});
+
+app.get('*', function(req, res) {
+	res.send(err404);
+});
+
+app.listen(port);
